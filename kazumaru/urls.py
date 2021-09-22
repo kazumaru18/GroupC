2fb078441472660f82febe73a0e5da5645bc05ef
kazumaru18 <k.tobe015@gmail.com>
2021-09-22 12:24:51 +0900

    blog_listの追加

--- kazumaru/urls.py
+++ kazumaru/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'kazumaru'
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('base/', views.base, name="base"),
+    path('index/', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('post/', views.post, name="post"),
+    path('contact/', views.contact, name="contact"),
 ]
